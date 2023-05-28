131a278e856793c12f4b4ce0ff628bac123bba38
cesar angulo <cesarangulo@cesars-MacBook-Air-2.local>
2023-05-27 19:07:12 -0700

    global component is callable down the pipeline to render components

--- src/SDK/Pay.jsx
+++ src/SDK/Pay.jsx
@@ -1,36 +1,77 @@
-
-// Import the necessary libraries
 import React from 'react';
-import {createPortal} from "react-dom"
-import {createRoot} from 'react-dom/client';
-import "./index.css"
+import ReactDOM from 'react-dom';
+import styled from 'styled-components';
+
+const ModalWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ModalContent = styled.div`
+  background: white;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+`;
+
+const PayButton = styled.button`
+  background: #007bff;
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 1rem;
+`;
 
 function PaymentComponent({ onPayment }) {
   const handlePayment = () => {
-    // ... some payment logic goes here...
-    // Let's say the payment was successful. Notify the parent:
     if (onPayment) {
       onPayment();
     }
   };
 
   return (
-    <button onClick={handlePayment}>
-      Pay Now
-    </button>
+    <ModalWrapper>
+      <ModalContent>
+        <h2>Payment Modal</h2>
+        <p>Payment details go here...</p>
+        <PayButton onClick={handlePayment}>Pay Now</PayButton>
+      </ModalContent>
+    </ModalWrapper>
   );
 }
 
+let modalContainer = null;
 
-// Import your component
-
-// Create a global object that exposes methods to render your components
 const MyLibrary = {
-  renderPaymentModal: (element, props, onPayment) => {
-    const root = createRoot(element)
-    let portal = createPortal(<PaymentComponent {...props} onPayment={onPayment} />, document.body);
-    root.render(portal)
+  initialize: (containerId) => {
+    modalContainer = document.getElementById(containerId);
+    if (!modalContainer) {
+      throw new Error('Could not find container element');
+    }
   },
+  renderPaymentModal: (props, onPayment) => {
+    if (!modalContainer) {
+      throw new Error('You must initialize MyLibrary with a container element before rendering the modal');
+    }
+
+    ReactDOM.render(<PaymentComponent {...props} onPayment={onPayment} />, modalContainer);
+  },
+  removePaymentModal: () => {
+    if (!modalContainer) {
+      throw new Error('You must initialize MyLibrary with a container element before removing the modal');
+    }
+
+    ReactDOM.unmountComponentAtNode(modalContainer);
+  }
 };
 
 export default MyLibrary;
--- src/payment_frontend/src/index.js
+++ src/payment_frontend/src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
 
-
+let PaymentModule
 
 window.onload = function() {
-  console.log("window",window)
-    var modalElement = document.getElementById('payment-modal');
-    ckPaySDK.PaymentComponent.renderPaymentModal(modalElement, { /* props go here */ });
+  ckPaySDK.PaymentComponent.initialize('payment-modal');
+
 }
 
 const App = () =>{
+  const handlePayment = () =>{
+    console.log("payment has been handle")
+  }
+
+ const startPaymentFlow = () =>{
+  ckPaySDK.PaymentComponent.renderPaymentModal({}, function() {
+    console.log('Payment complete');
+    // hide the modal when payment is done
+    ckPaySDK.PaymentComponent.removePaymentModal();
+  });
+  }
+
   return<div>
     <h1>hello world</h1>
+    <button onClick={()=>{
+  ckPaySDK.PaymentComponent.renderPaymentModal({}, function() {
+    // hide the modal when payment is done
+    ckPaySDK.PaymentComponent.removePaymentModal();
+  });}}>Pay in BTC</button>
     </div>
 }
 
